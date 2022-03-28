e8ce4978419502c39f85a5571596b12f18dde015
fox <o.citkowski@gmail.com>
2022-03-28 18:27:15 +0300

    add html topic page

--- daybook/urls.py
+++ daybook/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'daybook'
 urlpatterns = [
     # Mane page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #topics page
+    path('topics', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- daybook/views.py
+++ daybook/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic
 
 def index(reguest):
     ''' Mane page'''
     return render(reguest, 'daybook/index.html')
     # return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def topics(reguest):
+    ''' topics list page'''
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics' : topics}
+    return render(reguest, 'daybook/topics.html', context)
+
+
+def topic(reguest, topic_id):
+    ''' show topic single page'''
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic' : topic, 'entries':entries}
+    return render(reguest, 'daybook/topic.html', context)
