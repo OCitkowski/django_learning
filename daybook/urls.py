20e0bee650541a34c2cc99a31e472715cb60b722
fox <o.citkowski@gmail.com>
2022-04-11 17:38:24 +0300

    + Add all

--- daybook/urls.py
+++ daybook/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # Mane page
     path('', views.index, name='index'),
     #topics page
-    path('topics', views.topics, name='topics'),
+    path('topics/', views.topics, name='topics'),
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- daybook/views.py
+++ daybook/views.py
@@ -1,23 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Topic
+from .forms import TopicForm
 
-def index(reguest):
+
+def index(request):
     ''' Mane page'''
-    return render(reguest, 'daybook/index.html')
+    return render(request, 'daybook/index.html')
     # return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def topics(reguest):
+def topics(request):
     ''' topics list page'''
     topics = Topic.objects.order_by('date_added')
-    context = {'topics' : topics}
-    return render(reguest, 'daybook/topics.html', context)
+    context = {'topics': topics}
+    return render(request, 'daybook/topics.html', context)
 
 
-def topic(reguest, topic_id):
+def topic(request, topic_id):
     ''' show topic single page'''
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
-    context = {'topic' : topic, 'entries':entries}
-    return render(reguest, 'daybook/topic.html', context)
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'daybook/topic.html', context)
+
+
+def new_topic(request):
+    """Add new topic"""
+    if request.method != 'POST':
+        #         to create new empty form
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('daybook:topics')
+
+    context = {'form': form}
+    return render(request, 'daybook/new_topic.html', context)
