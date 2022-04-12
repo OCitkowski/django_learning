a5c36d977654c4918d908f307c4c0808f77eb891
fox <o.citkowski@gmail.com>
2022-04-12 10:34:44 +0300

    + add new entry

--- daybook/views.py
+++ daybook/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -38,3 +38,22 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'daybook/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """add new entry"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('daybook:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'daybook/new_entry.html', context)
+
+
