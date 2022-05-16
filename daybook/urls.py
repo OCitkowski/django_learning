f39b98f2437f7644dcfa71eef51dd358fa4d08d8
OCitkowski <o.citkowski@gmail.com>
2022-05-16 18:29:32 +0300

    add views model IndexView, EntryDetailView, TopicsListView, TopicDetailView

--- daybook/urls.py
+++ daybook/urls.py
@@ -1,15 +1,20 @@
 """Defines Url pattern for daybook"""
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from . import views
+from daybook.views import IndexView, TopicsListView, EntryDetailView, TopicDetailView
 
 app_name = 'daybook'
 urlpatterns = [
     # Mane page
-    path('', views.index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     #topics page
-    path('topics/', views.topics, name='topics'),
-    path('topics/<slug:topic_slug>/', views.topic, name='topic'),
+    # path('topics/', views.topics, name='topics'),
+    path('topics/', login_required(TopicsListView.as_view()), name='topics'),
+    # path('topic/<slug:topic_slug>/', views.topic, name='topic'),
+    path('topic/<slug:topic_slug>/', login_required(TopicDetailView.as_view()), name='topic'),
     path('new_topic/', views.new_topic, name='new_topic'),
     path('new_entry/<slug:topic_slug>/', views.new_entry, name='new_entry'),
     path('edit_entry/<slug:entry_slug>/', views.edit_entry, name='edit_entry'),
+    path('entry/<slug:entry_slug>/', login_required(EntryDetailView.as_view()), name='entry'),
 ]
--- daybook/views.py
+++ daybook/views.py
@@ -3,34 +3,85 @@ from django.http import Http404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Permission, User
+from django.views.generic import TemplateView, ListView, DetailView
+
+class IndexView(TemplateView):
+    template_name = 'daybook/index.html'
+
+class EntryDetailView(DetailView):
+    model = Entry
+    template_name = 'daybook/entry.html'
+    context_object_name = 'entry'
+    slug_url_kwarg = 'entry_slug'
+
+    def get_queryset(self):
+        return Entry.objects.filter(owner=self.request.user)
+
+    # return get_object_or_404(Entry.objects.filter(owner=self.request.user))
 
 
-def index(request):
-    ''' Mane page'''
-    return render(request, 'daybook/index.html')
-    # return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     ''' Mane page'''
+#     return render(request, 'daybook/index.html')
+#     # return HttpResponse("Hello, world. You're at the polls index.")
 
-@login_required
-def topics(request):
-    ''' topics list page'''
-    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
-    context = {'topics': topics}
-    return render(request, 'daybook/topics.html', context)
+class TopicsListView(ListView):
+    model = Topic
+    queryset = Topic.objects.order_by('date_added')
+    template_name = 'daybook/topics.html'
+    context_object_name = 'topics'
 
-@login_required
-def topic(request, topic_slug):
-    ''' show topic single page'''
-    for item in request:
-        print(item, topic_slug )
-    topic = Topic.objects.get(slug=topic_slug)
-    #
-    if topic.owner != request.user:
-        raise Http404
-    entries = topic.entry_set.filter(is_published=True).order_by('-date_added')
-    context = {'topic': topic, 'entries': entries}
-    return render(request, 'daybook/topic.html', context)
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        # context['topics'] = Topic.objects.all()
+        if len(Entry.objects.filter(owner=self.request.user)) == 0 :
+            redirect('topics/')
 
-@login_required
+        context['entries'] = Entry.objects.filter(owner=self.request.user)
+        return context
+
+    # def get_queryset(self):
+    #     return Topic.objects.filter(owner=self.request.user)
+
+# @login_required
+# def topics(request):
+#     ''' topics list page'''
+#     topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+#     context = {'topics': topics}
+#     return render(request, 'daybook/topics.html', context)
+
+# @login_required
+# def topic(request, topic_slug):
+#     ''' show topic single page'''
+#     topic = Topic.objects.get(slug=topic_slug)
+#     #
+#     # if topic.owner != request.user:
+#     #     raise Http404
+#     entries = topic.entry_set.filter(is_published=True).order_by('-date_added')
+#     context = {'topic': topic, 'entries': entries}
+#     return render(request, 'daybook/topic.html', context)
+
+class TopicDetailView(DetailView):
+
+    model = Topic
+    template_name = 'daybook/topic.html'
+    context_object_name = 'topic'
+    slug_url_kwarg = 'topic_slug'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Main page'
+        topic_id = Topic.objects.get(slug=self.kwargs['topic_slug']).id
+        entries = Entry.objects.filter(is_published=True, topic=topic_id, owner=self.request.user).order_by('-date_added')
+        context['entries'] = entries
+
+        return context
+
+
+# @login_required
 def new_topic(request):
     """Add new topic"""
     if request.method != 'POST':
@@ -70,14 +121,14 @@ def new_entry(request, topic_slug):
     context = {'topic': topic, 'form': form}
     return render(request, 'daybook/new_entry.html', context)
 
-@login_required
+# @login_required
 def edit_entry(request, entry_slug):
     """edit an existing entry."""
     # entry = Entry.objects.get(id=entry_id)
     entry = get_object_or_404(Entry, slug=entry_slug)
     topic = entry.topic
     #
-    if topic.owner != request.user:
+    if entry.owner != request.user:
         raise Http404
 
     if request.method != 'POST':
